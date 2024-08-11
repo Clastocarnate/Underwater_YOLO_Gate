1ea263bcbd3a8707564274513397fbcee28266b1
Clastocarnate <ayush2005au@gmail.com>
2024-08-11 19:56:46 +0530

    Import Dataset

---
+++ SLAMBook_Exercises/Chapter 2/eigenMatrix.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <ctime>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#define MATRIX_SIZE 50
+//argc = Argument count, argv = Argument Vector
+//Used for givign Arguments in the command line
+int main(int argc, char** argv){
+    //Eigen::Matrix is a template class which initialises matrix_23 to be a 2x3 matrix
+    Eigen::Matrix<float, 2, 3> matrix_23;
+    Eigen::Vector3d v_3d;
+    
+    
+    return 0;
+}
--- utils/vid2img.py
+++ utils/vid2img.py
@@ -2,9 +2,11 @@ import cv2
 import os
 
 vid = cv2.VideoCapture("IMG_0537.MOV")
-index = 0
+index = 1
 ret = 1
 
 while ret:
     ret, frame = vid.read()
+    cv2.imwrite(f"dataset/{index}.jpg",frame)
+    index += 1
     
