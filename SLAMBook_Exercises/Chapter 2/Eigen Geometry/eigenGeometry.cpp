fd6d7b231f41b8ae5b25bfa07cfecd0bb53950f1
Clastocarnate <ayush2005au@gmail.com>
2024-08-11 22:33:08 +0530

    Incomplete

---
+++ SLAMBook_Exercises/Chapter 2/Eigen Geometry/eigenGeometry.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+int main (int argc, char** argv){
+    
+    Eigen::Matrix3d rotation_matrix = Eigen::Matrix3d::Identity();
+    cout << rotation_matrix << endl;
+    Eigen::AngleAxisd rotation_vector(M_PI/4, Eigen::Vector3d (0,0,1));
+    cout .precision(3);
+    cout << rotation_vector.matrix();
+    rotation_matrix = rotation_vector.toRotationalMatrix();
+    Eigen::Vector3d v(1,0,0);
+    Eigen::Vector3d v_rotated = rotation_vector * v;
+    
+    return 0;
+}
