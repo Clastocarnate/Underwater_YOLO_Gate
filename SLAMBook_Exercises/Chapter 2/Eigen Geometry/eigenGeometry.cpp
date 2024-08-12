92065ef354944ae6d62bc88baaaa48cf8d73f110
Clastocarnate <ayush2005au@gmail.com>
2024-08-12 21:26:51 +0530

    Fixed Rotation Matrix Error

--- SLAMBook_Exercises/Chapter 2/Eigen Geometry/eigenGeometry.cpp
+++ SLAMBook_Exercises/Chapter 2/Eigen Geometry/eigenGeometry.cpp
@@ -11,9 +11,9 @@ int main (int argc, char** argv){
     Eigen::AngleAxisd rotation_vector(M_PI/4, Eigen::Vector3d (0,0,1));
     cout .precision(3);
     cout << rotation_vector.matrix();
-    rotation_matrix = rotation_vector.toRotationalMatrix();
+    rotation_matrix = rotation_vector.toRotationMatrix();
     Eigen::Vector3d v(1,0,0);
     Eigen::Vector3d v_rotated = rotation_vector * v;
-    
+    cout << "(1,0,0) after rotation is: " << v_rotated.transpose()<<endl;
     return 0;
 }
